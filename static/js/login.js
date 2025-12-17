6277c39a08db988b275ecc776f770659d4d9eceb
Jonathan Croteau-Dicaire <jonathan.croteau.dicaire@gmail.com>
2025-12-16 20:11:10 -0500

    Refactor login form handler for clarity
    Rename event handler parameter from e to error and format code.

--- static/js/login.js
+++ static/js/login.js
@@ -1,26 +1,28 @@
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const urlParams = new URLSearchParams();
-  for (const [key, value] of formData.entries()) {
-    urlParams.append(key, value);
-  }
-  const res = await fetch("/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: urlParams.toString(),
-    credentials: "include",
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async (error) => {
+    error.preventDefault();
+    const formData = new FormData(error.target);
+    const urlParams = new URLSearchParams();
+    for (const [key, value] of formData.entries()) {
+      urlParams.append(key, value);
+    }
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: urlParams.toString(),
+      credentials: "include",
+    });
+    if (response.status === 200 || response.status === 302) {
+      window.location.href = "/";
+    } else if (response.status === 401) {
+      const errorMessage = await response.text();
+      const errorParameters = document.getElementById("errorPara");
+      errorParameters.textContent = errorMessage;
+      errorParameters.style.display = "block";
+    } else {
+      console.warn("Unexpected response", response.status);
+    }
   });
-  if (res.status === 200 || res.status === 302) {
-    window.location.href = "/";
-  } else if (res.status === 401) {
-    const errMsg = await res.text();
-    const errorPara = document.getElementById("errorPara");
-    errorPara.textContent = errMsg;
-    errorPara.style.display = "block";
-  } else {
-    console.warn("Unexpected response", res.status);
-  }
-});
